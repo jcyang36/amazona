3d6e3d08b4991197f52efcfa9e4b07ae9bedcba0
jcyang36 <jcyang12358@gmail.com>
2020-12-26 17:21:30 -0500

    rendered using react

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products:[
+        {
+            name:'Best Shirt',
+            category:'Shirts',
+            image:'/images/shirt1.jpg',
+            price:60,
+            brand:'Nike',
+            rating:4.5,
+            numReview:10       
+        },
+        {
+            name:'Slim Shirt',
+            category:'Shirts',
+            image:'/images/shirt2.jpg',
+            price:50,
+            brand:'Nike',
+            rating:4.2,
+            numReview:10       
+        },
+        {
+            name:'Best Pants',
+            category:'Pants',
+            image:'/images/pants1.jpg',
+            price:80,
+            brand:'Nike',
+            rating:4.1,
+            numReview:10       
+        },
+        {
+            name:'Excellent Pants',
+            category:'Pants',
+            image:'/images/pants2.jpg',
+            price:80,
+            brand:'Nike',
+            rating:4.1,
+            numReview:10       
+        }
+    ]
+}
